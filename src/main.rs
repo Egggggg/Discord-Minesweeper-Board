37b8853c6b5040084fe94a38eabfb3c28fbe020c
Connor Horman <chorman64@gmail.com>
2020-03-01 22:12:55 -0500

    This works

---
+++ src/main.rs
@@ -0,0 +1,100 @@
+
+use std::iter::repeat;
+use rand::seq::SliceRandom;
+
+#[derive(Copy,Clone,Eq,PartialEq)]
+enum BoardState{
+    Empty,
+    Bomb,
+    Count(u8)
+}
+
+impl Default for BoardState{
+    fn default() -> Self {
+        Empty
+    }
+}
+
+use BoardState::*;
+use std::fmt::{Display, Formatter, Error};
+
+impl BoardState{
+    pub fn inc(&mut self){
+        match self{
+            Empty => *self = Count(1u8),
+            Count(v) => *v+=1,
+            _ => ()
+        }
+    }
+}
+
+impl Display for BoardState{
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), Error> {
+        match self{
+            Empty => f.write_str("||`  `||"),
+            Count(v) => f.write_fmt(format_args!("||`{} `||",v)),
+            Bomb => f.write_str("||`\u{1f4a3}`||")
+        }
+    }
+}
+
+const LEN: usize = 8;
+const WIDTH: usize = 8;
+const N_BOMBS: u8 = (WIDTH+LEN/2) as u8;
+
+struct Board{
+    state: [[BoardState;LEN];WIDTH]
+}
+
+impl Board{
+    pub fn gen(num_bombs: u8) -> Self{
+        let mut ret = Self{state: [[Empty;LEN];WIDTH]};
+        let mut indecies: Vec<(usize,usize)> = (0..WIDTH).flat_map(|n|repeat(n).zip(0..LEN)).collect();
+        indecies.shuffle(&mut rand::thread_rng());
+        for (x,y) in indecies.into_iter().take(num_bombs as usize){
+            ret.state[x][y] = Bomb;
+            if x+1 < WIDTH{
+                ret.state[x+1][y].inc();
+                if y+1 < LEN{
+                    ret.state[x+1][y+1].inc();
+                }
+                if y > 0{
+                    ret.state[x+1][y-1].inc();
+                }
+            }
+            if x > 0{
+                ret.state[x-1][y].inc();
+                if y+1 < LEN{
+                    ret.state[x-1][y+1].inc()
+                }
+                if y > 0{
+                    ret.state[x-1][y-1].inc()
+                }
+            }
+            if y+1 < LEN{
+                ret.state[x][y+1].inc();
+            }
+            if y > 0{
+                ret.state[x][y-1].inc();
+            }
+        }
+        ret
+    }
+}
+
+impl Display for Board{
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), Error> {
+        for v in self.state.iter(){
+            for s in v.iter(){
+                s.fmt(f)?;
+                f.write_str(" ")?;
+            }
+            f.write_str("\n")?;
+        }
+        Ok(())
+    }
+}
+
+fn main() {
+    println!("{}",Board::gen(N_BOMBS));
+}
